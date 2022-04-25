61a0988b89f96fe3e1ae5e019f630cd7a964fe16
aronei44 <meliodasmeliodas224@gmail.com>
2022-04-25 14:14:23 +0700

    feat (backend) : add login, register and logout with sanctum

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,4 +12,53 @@ class AuthController extends Controller
     {
         return Inertia::render('Auth/Index');
     }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Password does not match',
+            ], 401);
+        } else {
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'user' => $user,
+            ],200);
+        }
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ],201);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ],401);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/login', [Auth\AuthController::class, 'login']);
+Route::post('/register', [Auth\AuthController::class, 'register']);
+Route::post('/logout', [Auth\AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
