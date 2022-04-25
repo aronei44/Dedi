6d694848192d507d5c4f619a6b1e477f491af124
aronei44 <meliodasmeliodas224@gmail.com>
2022-04-25 10:34:48 +0700

    feat (frontend) : add inertia js

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <h1>Welcome</h1>
+            <p>Hello, welcome to your first Inertia app!</p>
+        </>
+    )
+}
