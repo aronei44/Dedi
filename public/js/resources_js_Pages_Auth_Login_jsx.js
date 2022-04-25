535f848fbc0e694d2cd0b327e0bb71f9f7ed80dd
aronei44 <meliodasmeliodas224@gmail.com>
2022-04-25 10:40:43 +0700

    feat (Frontend) : Add BrowserSync for auto refresh

--- public/js/resources_js_Pages_Auth_Login_jsx.js
+++ public/js/resources_js_Pages_Auth_Login_jsx.js
@@ -1,15 +1,15 @@
 "use strict";
-(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Index_jsx"],{
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Auth_Login_jsx"],{
 
-/***/ "./resources/js/Pages/Index.jsx":
-/*!**************************************!*\
-  !*** ./resources/js/Pages/Index.jsx ***!
-  \**************************************/
+/***/ "./resources/js/Pages/Auth/Login.jsx":
+/*!*******************************************!*\
+  !*** ./resources/js/Pages/Auth/Login.jsx ***!
+  \*******************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ Index)
+/* harmony export */   "default": () => (/* binding */ Login)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var _inertiajs_inertia_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @inertiajs/inertia-react */ "./node_modules/@inertiajs/inertia-react/dist/index.js");
@@ -19,10 +19,10 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-function Index() {
+function Login() {
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.Fragment, {
     children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_inertiajs_inertia_react__WEBPACK_IMPORTED_MODULE_1__.Head, {
-      title: "Index"
+      title: "Megamendung - Login"
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("h1", {
       children: "Welcome gas"
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("p", {
--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Head } from '@inertiajs/inertia-react'
 
-export default function Welcome() {
+export default function Index() {
     return (
         <>
-            <Head title="Welcome" />
-            <h1>Welcome</h1>
+            <Head title="Index" />
+            <h1>Welcome gas</h1>
             <p>Hello, welcome to your first Inertia app!</p>
         </>
     )
